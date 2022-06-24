5df68992a462c4983c4d8df8e1b14c4e9890b4c7
Alina <pisarenko.alina@gmail.com>
2022-06-24 12:43:07 -0400

    resolving the merging conflict

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -2,16 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Review from "./Review";
 import "../sass/project.scss";
-
 export default function Project(user) {
   const [projectData, setProjectData] = useState([]);
   const [allReviews, setAllReviews] = useState([]);
   const [reviewShow, setReviewShow] = useState(false);
   const { id } = useParams();
-
   const { name, medium, image, theme, description, price, reviews } =
     projectData;
-
   function getProjectInfoFetch() {
     fetch(`/projects/${id}`)
       .then((res) => {
@@ -28,11 +25,36 @@ export default function Project(user) {
         console.log(error);
       });
   }
-
-  function handleAddReview() {
+  useEffect(() => {
+    getProjectInfoFetch();
+  }, []);
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    let configObj = {
+      content: e.target.content.value,
+      user_id: user.user.id,
+      post_id: +id,
+    };
+    let response = await fetch(`/review`, {
+      method: `POST`,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(configObj),
+    });
+    if (response.ok) {
+      let review = await response.json();
+      handleAddReview();
+      //   reviewsToDisplay.push(review)
+      setAllReviews((allReviews) => [...allReviews, review]);
+    } else {
+      console.log(await response.json());
+    }
+  };
+  function handleAddReview(e) {
+    e.preventDefault();
     setReviewShow((reviewShow) => !reviewShow);
   }
-
   if (projectData.length < 1) {
     return <div className="App">Loading...</div>;
   } else {
@@ -60,87 +82,50 @@ export default function Project(user) {
                 <span className="project-info-row">${price}</span>
               </div>
             </div>
-            <div className="info-container">
-              <div>
-                <div className="project-sep">
-                  <h1>Name: </h1>
-                  <span className="project-info-row">{name}</span>
-                  <h1>Medium: </h1>
-                  <span className="project-info-row">{medium}</span>
-                </div>
-                <div className="project-sep">
-                  <h1>Theme: </h1>
-                  <span className="project-info-row">{theme}</span>
-                  <h1>Price: </h1>
-                  <span className="project-info-row">${price}</span>
-                </div>
-              </div>
-              <h1>Description: </h1>
-              <ul className="project-info-row">
-                {description} Lorem ipsum dolor sit amet, consectetur adipiscing
-                elit. Praesent ornare velit in justo vehicula rhoncus. Maecenas
-                semper, quam in ultricies pharetra, metus tellus sodales elit,
-                ut malesuada nunc eros vel lacus. Interdum et malesuada fames ac
-                ante ipsum primis in faucibus. Sed tellus tortor, tempus in
-                consequat nec, molestie nec dolor. Pellentesque laoreet nisl ac
-                nisi imperdiet vestibulum. Donec varius eros vel urna lacinia,
-                non pellentesque risus varius. Integer sit amet euismod neque.
-                Vestibulum cursus placerat mauris id consectetur. Nam et nisi a
-                est imperdiet accumsan. Etiam ut hendrerit elit. Sed venenatis
-                nulla nisl, nec tincidunt tortor lobortis quis. Vivamus rhoncus,
-                elit ut pretium dictum, mauris justo consectetur nulla, at
-                elementum sem dui vel odio. Aliquam dapibus sed odio vestibulum
-                porttitor. Cras eget tortor vel velit rhoncus dictum.
-              </ul>
-            </div>
+            <h1>Description: </h1>
+            <ul className="project-info-row">
+              {description} Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit. Praesent ornare velit in justo vehicula rhoncus. Maecenas
+              semper, quam in ultricies pharetra, metus tellus sodales elit, ut
+              malesuada nunc eros vel lacus. Interdum et malesuada fames ac ante
+              ipsum primis in faucibus. Sed tellus tortor, tempus in consequat
+              nec, molestie nec dolor. Pellentesque laoreet nisl ac nisi
+              imperdiet vestibulum. Donec varius eros vel urna lacinia, non
+              pellentesque risus varius. Integer sit amet euismod neque.
+              Vestibulum cursus placerat mauris id consectetur. Nam et nisi a
+              est imperdiet accumsan. Etiam ut hendrerit elit. Sed venenatis
+              nulla nisl, nec tincidunt tortor lobortis quis. Vivamus rhoncus,
+              elit ut pretium dictum, mauris justo consectetur nulla, at
+              elementum sem dui vel odio. Aliquam dapibus sed odio vestibulum
+              porttitor. Cras eget tortor vel velit rhoncus dictum.
+            </ul>
           </div>
-          <Review
-            user={user}
-            allReviews={allReviews}
-            setAllReviews={setAllReviews}
-          />
-          {reviewShow ? (
-            <div className="add-review-container">
-              <form id="add-review-form" onSubmit={handleFormSubmit}>
-                <p>Enter review: </p>
-                <textarea
-                  rows="5"
-                  cols="50"
-                  name="content"
-                  type="content"
-                  placeholder="What do you think of this piece?"
-                />
-                <button className="add-review-btn" type="submit">
-                  +
-                </button>
-              </form>
-            </div>
-          ) : null}
-          <h4 className="add-review" onClick={handleAddReview}>
-            Add new review +
-          </h4>
         </div>
         <Review
           user={user}
           allReviews={allReviews}
           setAllReviews={setAllReviews}
         />
-        <h4 className="add-review" onClick={handleAddReview}>
-          Add new review +
-        </h4>
         {reviewShow ? (
-          <div>
+          <div className="add-review-container">
             <form id="add-review-form" onSubmit={handleFormSubmit}>
               <p>Enter review: </p>
-              <input
+              <textarea
+                rows="5"
+                cols="50"
                 name="content"
                 type="content"
                 placeholder="What do you think of this piece?"
               />
-              <button type="submit">Add review</button>
+              <button className="add-review-btn" type="submit">
+                +
+              </button>
             </form>
           </div>
         ) : null}
+        <h4 className="add-review" onClick={handleAddReview}>
+          Add new review +
+        </h4>
       </div>
     );
   }
